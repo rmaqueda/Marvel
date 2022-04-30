46b9ced039818f8b079a14a496af928bb68fc50d
Ricardo Maqueda <rinconestolmo@gmail.com>
2022-04-30 09:32:40 +0200

    Add Xcode project

---
+++ MarvelAPI/MarvelAPI/Hello.swift
@@ -0,0 +1,14 @@
+//
+//  Hello.swift
+//  MarvelAPI
+//
+//  Created by Maqueda, Ricardo Javier on 30/4/22.
+//
+
+import Foundation
+
+public struct Hello {
+    public init() {
+        print("Hello from Marvel API")
+    }
+}
